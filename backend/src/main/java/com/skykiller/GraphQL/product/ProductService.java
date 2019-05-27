d97fcd969348f7b7deed8b38ab1ce1fbc518b021
John Skiller <john@skykiller.com>
2019-05-28 08:58:05 +0900

    try log sql time

--- backend/src/main/java/com/skykiller/GraphQL/product/ProductService.java
+++ backend/src/main/java/com/skykiller/GraphQL/product/ProductService.java
@@ -2,18 +2,22 @@ package com.skykiller.GraphQL.product;
 
 import java.util.List;
 
-import org.springframework.stereotype.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 class ProductService {
     private ProductRepository repository;
-    
+    private static final Logger logger = LoggerFactory.getLogger(ProductService.class);
+
     // use constructer injection
     ProductService(ProductRepository repository) {
         this.repository = repository;
     }
     
     List<Product> getAll() {
+        logger.info("get all products");
         return repository.findAll();
     }
 
